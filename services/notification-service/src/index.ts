ff5abdb0861b38fefb119423fab540c69ea98811
Jeffrey Bernadas <jeffrey.bernadas0@gmail.com>
2025-05-08 12:43:27 +0800

    Feat: Add api version for the Notification Service

--- services/notification-service/src/index.ts
+++ services/notification-service/src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import healthCheckHandler from "@notifications/routes/health.route";
 import { APP_ORIGIN, PORT } from "@notifications/constants/env.constants";
+import { API_PREFIX, API_VERSION } from "@notifications/constants/version.constant";
 
 const app = express();
 
@@ -10,8 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: APP_ORIGIN, credentials: true }));
 
-app.get("/health", healthCheckHandler);
+app.get(`${API_PREFIX}/health`, healthCheckHandler);
 
 app.listen(PORT, () => {
-  console.log(`Auth Service is running on port ${PORT}.`);
+  console.log(`Notification Service (${API_VERSION}) is running on port ${PORT}.`);
 });
